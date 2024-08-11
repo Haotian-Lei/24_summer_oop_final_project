3d3a0e1b3906d4944a72eff036c4247c40c4dc02
Yukying <yc5099@nyu.edu>
2024-08-11 05:53:17 -0400

    Menu class finished

--- Menu.java
+++ Menu.java
@@ -1,4 +1,40 @@
 
-public class Menu {
+import java.io.Serializable;
+import java.util.List;
+import java.util.ArrayList;
 
+public class Menu implements Serializable {
+	private List<MenuItem> menuitems;
+	public Menu() {
+		this.menuitems = new ArrayList<>();
+	}
+	public void addMenuItem(MenuItem mi) {
+		this.menuitems.add(mi);
+	}
+	public  void deleteMenuItem(int index) {
+		this.menuitems.remove(index);
+	}
+	public List<MenuItem> getAllMenuItems(){
+		return menuitems;
+	}
+	public String getNameMenuItem(int index) {
+		return menuitems.get(index).getName();
+	}
+	public double getPriceMenuItem(int index) {
+		return menuitems.get(index).getPrice();
+	}
+	public void editMenuItem(int index, String name, double price) {
+		MenuItem mi = menuitems.get(index);
+		mi.setName(name);
+		mi.setPrice(price);
+	}
+	public String toString() {
+		String str = "";
+		for (MenuItem mi: menuitems) {
+			str+= mi.getName()+ " "+mi.getPrice()+"\n";
+		}
+		return str;
+	}
+	
 }
+
